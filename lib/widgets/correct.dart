8e42efc2be550159c70385403c71621abf49f094
PhysicalRevelry <kristinnewmancscs@gmail.com>
2021-05-25 19:42:44 -0500

    working on animations

--- lib/widgets/correct.dart
+++ lib/widgets/correct.dart
@@ -1,6 +1,4 @@
-import 'dart:async';
-
-import 'package:flutter/material.dart';
+ import 'package:flutter/material.dart';
 import 'package:hotkey_flashcards/state/state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -8,18 +6,23 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 class CorrectWidget extends HookWidget {
   const CorrectWidget({Key key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
+    final ticker = useSingleTickerProvider();
     final flashcard = useProvider(stateProvider);
     flashcard.exchangeWidget();
     return Container(
       child: Column(
         children: [
-          Icon(
-            Icons.check,
-            color: Colors.green,
-            size: 86,
+          AnimatedSize(
+            curve: Curves.bounceIn,
+            duration: Duration(seconds: 1),
+            vsync: ticker,
+            child: Icon(
+              Icons.check,
+              color: Colors.green,
+              size: 86,
+            ),
           ),
           Text(flashcard.keyedHotKey.keyName),
           flashcard.keyedHotKey.alt ? Text("Alt") : SizedBox(height: 0),
