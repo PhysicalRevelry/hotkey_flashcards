9ba107112432514f12573ba09012400cec4ff134
PhysicalRevelry <kristinnewmancscs@gmail.com>
2021-06-03 15:12:12 -0500

    Added some Expanded widgets with flex factors to set spacing.

--- lib/screens/flashcards.dart
+++ lib/screens/flashcards.dart
@@ -3,6 +3,7 @@ import 'package:hotkey_flashcards/state/state.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hotkey_flashcards/widgets/choose_verdict.dart';
+import 'package:hotkey_flashcards/widgets/display_target.dart';
 
 class FlashCards extends HookWidget {
   const FlashCards({Key? key}) : super(key: key);
@@ -22,26 +23,32 @@ class FlashCards extends HookWidget {
             padding: EdgeInsets.symmetric(horizontal: 100),
             child: Center(
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text(
-                    'ENTER YOUR HOTKEY',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 42,
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: EdgeInsets.all(20.0),
+                      child: Text(
+                        'ENTER YOUR HOTKEY',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 42,
+                        ),
+                      ),
                     ),
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  Text(
-                    flashCard.target == null
-                        ? " "
-                        : "${flashCard.target!.label}\n\n${flashCard.target!.description}",
-                    textAlign: TextAlign.center,
+                  // SizedBox(
+                  //   height: 30,
+                  // ),
+                  Expanded(
+                    flex: 1,
+                    child: DisplayTarget(),
                   ),
                   //TODO Also a widget,
-                  VerdictWidget(),
+                  Expanded(
+                    flex: 2,
+                    child: VerdictWidget(),),
                 ],
               ),
             ),
--- lib/widgets/display_target.dart
+++ lib/widgets/display_target.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:hotkey_flashcards/state/state.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
-class DisplayTargetWidget extends StatelessWidget {
-  const DisplayTargetWidget({Key? key}) : super(key: key);
+class DisplayTarget extends HookWidget {
+  const DisplayTarget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final flashCard = useProvider(stateProvider);
+    return Text(
+      flashCard.target == null
+          ? " "
+          : "${flashCard.target!.label}\n\n${flashCard.target!.description}",
+      textAlign: TextAlign.center,
+    );
   }
 }
