a58743eba1ec2e3bed0d479fb1439c01cefe324c
Kristin Newman <kristinnewmancscs@gmail.com>
2021-06-15 13:28:53 -0500

    Adding more hotkeys. Up to 14

--- lib/hotkeys/key_map.dart
+++ lib/hotkeys/key_map.dart
@@ -1,9 +1,23 @@
 import 'hotkeys.dart';
 
+final commentOut = HotKey(
+    label: 'Comment Out Line',
+    description:
+        'Comments out a line or block of code or removes the comment slashes to activate commented code',
+    keyName: 'Slash',
+    meta: true);
+//TODO what is the keyname for the slash??
 final commitCode = HotKey(
     label: 'Commit Code',
     description: 'Git is already set up and you want to make a commit',
     keyName: 'Key K',
+    meta: true);
+final completeStatement = HotKey(
+    label: 'Complete Statement',
+    description:
+        'Can finish your statement such as putting the final semicolon or even finishing the syntax for a for loop or if statement',
+    keyName: "Enter",
+    shift: true,
     meta: true);
 final doTheRightThing = HotKey(
     label: 'Do the right thing',
@@ -13,14 +27,14 @@ final doTheRightThing = HotKey(
 final expandEditor = HotKey(
     label: 'Expand Editor',
     description:
-    'Maximizes and minimizes the editing window by showing or hiding the side navigation',
+        'Maximizes and minimizes the editing window by showing or hiding the side navigation',
     keyName: 'F12',
     meta: true,
     shift: true);
 final fileNavigation = HotKey(
     label: 'File Navigation',
     description:
-    'Opens a menu to navigate to recently open tabs or other files and folders, starting with most recent',
+        'Opens a menu to navigate to recently open tabs or other files and folders, starting with most recent',
     keyName: 'Key E',
     meta: true);
 final formatCode = HotKey(
@@ -30,22 +44,39 @@ final formatCode = HotKey(
     alt: true,
     meta: true);
 final getDocumentation = HotKey(
-    label: 'Track Usage',
+    label: 'Find Usage',
     description:
-    'Finds variable or function usage or digs into element documentation',
+        'Finds variable or function usage or digs into element documentation',
     keyName: 'Key B',
+    meta: true);
+final navigateErrors = HotKey(
+    label: 'Navigate Errors',
+    description: 'Navigate between errors in a file',
+    keyName: 'F2');
+final projectFolder = HotKey(
+    label: 'Project Folder Navigation',
+    description:
+        'Opens and focuses on project folder so you can navigate using arrow keys, or closes project folder and focuses your editor',
+    keyName: 'Digit 1',
     meta: true);
 final refactorName = HotKey(
     label: 'Naming Refactor',
     description:
-    'Change the name of a variable or class everywhere it is mentioned',
+        'Change the name of a variable or class everywhere it is mentioned',
     keyName: 'F6',
     shift: true);
+final refactorThis = HotKey(
+    label: 'Refactor This',
+    description:
+        'Refactor the element under the caret, for example, safe delete, copy, move, rename, and so on',
+    keyName: 'Key T',
+    control: true);
 final saveAll = HotKey(
     label: 'Save',
     description: 'Save everything you\'ve done up to that point',
     keyName: 'Key S',
     control: true);
+
 /// Search Everywhere is a double shift, not sure how to even code that!
 /// But it's such a cool shortcut to have!
 // final searchEverywhere = HotKey(label: 'Search Everywhere',
@@ -59,24 +90,27 @@ final selectCodeBlock = HotKey(
 final suggestAction = HotKey(
     label: 'Suggest an Action',
     description:
-    'Like "do the right thing," it will bring up a menu of options',
+        'Like "do the right thing," it will bring up a menu of options',
     keyName: 'Space',
     control: true);
 final tabNavigation = HotKey(
     label: 'Tab Navigation',
     description:
-    'Navigate between open tabs, with more recently used tabs at the top of the list',
+        'Navigate between open tabs, with more recently used tabs at the top of the list',
     keyName: 'Key E',
     control: true);
 
 final List<HotKey> listOfHotKeys = [
   commitCode,
+  completeStatement,
   doTheRightThing,
   expandEditor,
   fileNavigation,
   formatCode,
   getDocumentation,
+  navigateErrors,
   refactorName,
+  refactorThis,
   saveAll,
   selectCodeBlock,
   suggestAction,
