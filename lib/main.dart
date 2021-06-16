cf7d5fd2849f26d564a9c18b79625c9a6e110057
Kristin Newman <kristinnewmancscs@gmail.com>
2021-06-16 14:13:16 -0500

    Set up min/max window size :D

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:hotkey_flashcards/const/theme.dart';
 import 'package:hotkey_flashcards/screens/flashcards.dart';
 import 'package:hotkey_flashcards/screens/get_started.dart';
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    // setWindowTitle('App title');
+    setWindowMinSize(const Size(700, 700));
+    setWindowMaxSize(Size.infinite);
+  }
   runApp(ProviderScope(child: MyApp()));
 }
 
