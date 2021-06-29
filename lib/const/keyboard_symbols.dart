705fab5006a6d9253e594c458d6b36407a7a7e71
PhysicalRevelry <kristinnewmancscs@gmail.com>
2021-06-29 14:02:14 -0500

    Adding these instead of icons

---
+++ lib/const/keyboard_symbols.dart
@@ -0,0 +1,9 @@
+
+class KeyboardSymbols {
+  final String alt = "\u2387";
+  final String option = "\u2318";
+  final String enter = "\u2386";
+  final String shift = "\u21e7";
+  final String control = "\u2303";
+
+}
